fa234ce54c97e44f64e6782c44065dbdf34a4788
Hassan Abedi <hassan.abedi.t@gmail.com>
2015-05-21 22:33:38 +0430

    Timer.py added

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Hmm ... practice makes perfect they say, trying to better my Python coding a lit
 
   
 1. Bipartite Graphs in Igraph (Py2) {codes/bipartite.py}
+2. A Timer class to find the running time of a of piece of code in Python {codes/Timer.py}
 
 
 ---
---
+++ codes/Timer.py
@@ -0,0 +1,15 @@
+import time
+
+class Timer(object):
+    def __init__(self, name=None):
+        self.name = name
+
+    def __enter__(self):
+        self.tstart = time.time()
+
+    def __exit__(self, type, value, traceback):
+        if self.name:
+            print (str(self.name))
+        print ('Elapsed time is: ' + str(time.time() - self.tstart))
+        pass
+    pass
