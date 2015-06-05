50546aefa669c08530bb863f15b4c10bf40d8c63
Hassan Abedi <hassan.abedi.t@gmail.com>
2015-06-05 20:54:53 +0430

    Stats.py has a new method now

--- codes/Stats.py
+++ codes/Stats.py
@@ -1,6 +1,26 @@
+from __future__ import print_function
+
 
 # my statistical class
 class Stats(object):
     def __init__(self,):
         pass
-    pass
+    
+    def getCD(self, data_points):
+        """ this method takes a list of values and return a dictionary containing
+            the comulative distribution of each item in the list
+            {k=item, v=times seen in the list} """
+        
+        if isinstance(data_points, list):
+            freq = {}
+            for value in data_points:
+                if value not in freq:
+                    freq.update({value:1})
+                else:
+                    freq[value] += 1
+            return freq
+        return None
+
+# test and debug
+s = Stats()
+print (s.getCD(data_points=[1,1,2,3,45,8,2,0,0.2,-8]))
