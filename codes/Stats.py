1e23e04b693f8a284abf3d4d956904b44c238068
Hassan Abedi <hassan.abedi.t@gmail.com>
2015-06-06 22:28:47 +0430

    My simple Statistic class is being developed

--- codes/Stats.py
+++ codes/Stats.py
@@ -3,24 +3,37 @@ from __future__ import print_function
 
 # my statistical class
 class Stats(object):
-    def __init__(self,):
-        pass
+    def __init__(self, seq):
+        #seq must be a list of real valued numbers
+        if isinstance(seq, list):
+            self.seq = seq
+        else:
+            raise Exception
     
-    def getCD(self, data_points):
-        """ this method takes a list of values and return a dictionary containing
-            the comulative distribution of each item in the list
+    def getCD(self,):
+        """ this method process a list of values and return a dictionary containing
+            the comulative distribution of each item in the self.seq
             {k=item, v=times seen in the list} """
-        
-        if isinstance(data_points, list):
-            freq = {}
-            for value in data_points:
-                if value not in freq:
-                    freq.update({value:1})
-                else:
-                    freq[value] += 1
-            return freq
-        return None
+        freq = {}
+        for value in self.seq:
+            if value not in freq:
+                freq.update({value:1})
+            else:
+                freq[value] += 1
+        return freq
+
+    def getMean(self,):
+        return sum(self.seq)/len(self.seq)
+
+    def getMode(self,):
+        d = self.getCD()
+        return max(d, key=d.get)
+    
+    def getMedian(self,):
+        pass
 
 # test and debug
-s = Stats()
-print (s.getCD(data_points=[1,1,2,3,45,8,2,0,0.2,-8]))
+s = Stats(seq=[1,1,2,3,45,8,2,0,0.2,-8])
+print (s.getCD())
+print (s.getMean())
+print (s.getMode())
