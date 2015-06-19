5051f07085f6ec19fd70dfb8f766b760e9d20a90
Hassan Abedi <hassan.abedi.t@gmail.com>
2015-06-19 23:48:05 +0430

    Normalized Compression Distance class in Python

---
+++ codes/NCD.py
@@ -0,0 +1,10 @@
+
+# Normalized Compression Distance
+class NCD(object):
+    def __init__(self,):
+        pass
+    def compress(self, infile):
+        pass
+    def ncd(self,):
+        pass
+    pass
